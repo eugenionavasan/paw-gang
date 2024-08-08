bae2c86910c69c0f077a09def76baebbe7e5a9d9
andre-amato <andreamatoprs@gmail.com>
2024-08-08 05:13:26 -0300

    some changes

--- client/config.ts
+++ client/config.ts
@@ -2,6 +2,6 @@ import Constants from 'expo-constants';
 
 const { extra } = Constants.expoConfig || {};
 
-export const LOCAL_IP_ADDRESS: string = extra?.localIpAddress || '192.168.0.73';
+export const LOCAL_IP_ADDRESS: string = extra?.localIpAddress || '192.168.0.208';
 export const GOOGLE_MAPS_API_KEY: string = extra?.googleMapsApiKey || '';
-export const SERVER_PORT: number | string = extra?.serverPort || 3002;
+export const SERVER_PORT: number | string = extra?.serverPort || 3001;
